059111025286c9fe60b3a6dacd9a3a61331fcc37
calebjo <caleb.ri.jones@gmail.com>
2021-12-25 01:32:31 -0500

    Add structure in index.js

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div>
-          <section><h1>Section 1</h1></section>
-          <section><h1>Section 2</h1></section>
-          <section><h1>Section 3</h1></section>
-          <section><h1>Section 4</h1></section>
-        </div>
-      </main>
+      <div className={styles.pages}>
+        <section><h1>Section 1</h1></section>
+        <section><h1>Section 2</h1></section>
+        <section><h1>Section 3</h1></section>
+        <section><h1>Section 4</h1></section>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.footer}>
+        <h2>Hi, I'm the footer!</h2>
+      </div>
     </div>
   )
 }
