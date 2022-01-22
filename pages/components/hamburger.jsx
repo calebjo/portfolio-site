7d0a97e4adf663c0bba18a11b5f05cb88ea00d3f
calebjo <caleb.ri.jones@gmail.com>
2022-01-22 16:29:56 -0500

    Fix bugs in deploy

--- pages/components/hamburger.jsx
+++ pages/components/hamburger.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import styles from '../../styles/Home.module.css'
 
 export default class Hamburger extends React.Component {
     constructor(props){
--- pages/components/topNav.jsx
+++ pages/components/topNav.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
+import Hamburger from './hamburger'
 
 export default function TopNav() {
     return(
