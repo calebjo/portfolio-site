529e2920bfa02498bc3de547682463a001aaff4b
calebjo <caleb.ri.jones@gmail.com>
2022-01-28 12:52:51 -0500

    Add animations to dropdown

--- pages/components/hamburger.jsx
+++ pages/components/hamburger.jsx
@@ -4,15 +4,29 @@ import styles from '../../styles/Home.module.css'
 export default class Hamburger extends React.Component {
     constructor(props){
         super(props)
-        this.state = { toggled: false }
+        this.state = { 
+            toggled: false,
+            fade: false
+        }
         this.toggleDropdown = this.toggleDropdown.bind(this)
     }
 
     toggleDropdown(event){
-        event.currentTarget.parentNode.classList.toggle("toggled")
-        this.setState({
-            toggled: true
-        })
+        if (this.state.toggled) {
+            this.setState({
+                fade: true
+            })
+            setTimeout(() => {
+                this.setState({
+                    fade: false,
+                    toggled: false
+                })
+            }, 75)
+        } else {
+            this.setState({
+                toggled: true
+            })
+        }
     }
     
     hoverDropdown(event){
@@ -20,8 +34,27 @@ export default class Hamburger extends React.Component {
     }
 
     render(){
+        const navClasses = this.state.toggled ? (
+            this.state.fade ? (
+                'nav-dropdown toggled fade'
+            ) : (
+                'nav-dropdown toggled'
+            )
+        ) : (
+            'nav-dropdown'
+        )
         return (
-            <div className="nav-dropdown">
+            <div className={navClasses}>
+                <div className="dropdown-container">
+                    <a href="#home" onClick={this.toggleDropdown}><span>Home</span></a>
+                    <div className="dropdown-link-separator" />
+                    <a href="#skills" onClick={this.toggleDropdown}><span>Skills</span></a>
+                    <div className="dropdown-link-separator" />
+                    <a href="#projects" onClick={this.toggleDropdown}><span>Projects</span></a>
+                    <div className="dropdown-link-separator" />
+                    <a href="#contact" onClick={this.toggleDropdown}><span>Contact</span></a>
+                    <div className="dropdown-link-separator" />
+                </div>
                 <div 
                     className="hamburger"
                     onClick={this.toggleDropdown} 
@@ -31,12 +64,6 @@ export default class Hamburger extends React.Component {
                     <div className="bar1"></div>
                     <div className="bar2"></div>
                     <div className="bar3"></div>
-                </div>
-                <div className="dropdown-container">
-                    <a href="#home">Home</a>
-                    <a href="#skills">Skills</a>
-                    <a href="#projects">Projects</a>
-                    <a href="#contact">Contact</a>
                 </div>
             </div>
             
